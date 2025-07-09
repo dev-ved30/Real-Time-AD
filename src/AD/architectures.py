e0e69eebbe069ba9140f250b78c9e8bbde845875
ved <vedshah.email@gmail.com>
2025-07-09 02:11:02 -0500

    Added new MM architecture using the swin2 transformer

--- src/AD/architectures.py
+++ src/AD/architectures.py
@@ -165,25 +165,93 @@ class GRU(Classifier):
 
         return logits
     
+class GRU_MM(Classifier):
+
+    def __init__(self, output_dim, ts_feature_dim=5, static_feature_dim=18):
+
+        super(GRU_MM, self).__init__()
+
+        self.ts_feature_dim = ts_feature_dim
+        self.static_feature_dim = static_feature_dim
+        self.output_dim = output_dim
+
+        # TODO: Think about what weights we want to initialize the transformer with.
+        self.swin_postage = torch.hub.load("pytorch/vision", "swin_v2_t", weights="DEFAULT", progress=False)
+
+        # Make sure all parameters in Swin are trainable
+        for param in self.swin_postage.parameters():
+            param.requires_grad = True
+
+        # recurrent backbone
+        self.gru = nn.GRU(input_size=ts_feature_dim, hidden_size=100, num_layers=2, batch_first=True)
+
+        # post‐GRU dense on time‐series path
+        self.dense1 = nn.Linear(100, 100)
+
+        # dense on static path
+        self.dense2 = nn.Linear(static_feature_dim, 10)
+
+        self.swin_fc = nn.Sequential(
+            nn.Linear(1000, 512),
+            nn.ReLU(True),
+            nn.Linear(512, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 64),
+            nn.ReLU(True),
+        )
+
+        # merge & head
+        self.dense3 = nn.Linear(100 + 10 + 64, 100)
+        self.dense4 = nn.Linear(100, 64)
+
+        self.fc_out = nn.Linear(64, self.output_dim)
+
+        self.tanh = nn.Tanh()
+        self.relu = nn.ReLU()
+    
     def get_latent_space_embeddings(self, batch):
 
-        embeddings = {}
+        x_ts = batch['ts'] # (batch_size, seq_len, n_ts_features)
+        lengths = batch['length'] # (batch_size)
+        x_static = batch['static'] # (batch_size, n_static_features)
+        x_postage = batch['postage_stamp']
 
-        def save_latent(module, inp, out):
-            # out is the dense4 output
-            embeddings['latent'] = out.detach()
+        # Pack the padded time series data. the lengths vector lets the GRU know the true lengths of each TS, so it can ignore padding
+        packed = pack_padded_sequence(x_ts, lengths.cpu(), batch_first=True, enforce_sorted=False)
 
-        # register hook
-        hook_handle = self.dense2.register_forward_hook(save_latent)
+        # Recurrent backbone
+        h0 = torch.zeros(2, x_ts.shape[0], 100).to(x_ts.device)
+        _, hidden = self.gru(packed, h0)
 
-        # run your forward
-        with torch.no_grad():
-            preds = self.forward(batch)
+        # Take the last output of the GRU
+        gru_out = hidden[-1] # (batch_size, hidden_size)
 
-        # now embeddings['latent'] holds the (batch, latent_space_dim) tensor
-        latent_vectors = embeddings['latent']
+        swin_out = self.swin_postage(x_postage)
+        swin_fc_out = self.swin_fc(swin_out)
 
-        # when done, remove the hook to avoid memory leaks
-        hook_handle.remove()
-        return latent_vectors
+        # Post-GRU dense on time-series path
+        dense1 = self.dense1(gru_out)
+        dense1 = self.tanh(dense1)
+
+        # Dense on static path
+        dense2 = self.dense2(x_static)
+        dense2 = self.tanh(dense2)
+
+        # Merge & head
+        x = torch.cat((dense1, dense2, swin_fc_out), dim=1)
+        x = self.dense3(x)
+        x = self.relu(x)
+        x = self.dense4(x)
+
+        return x
     
+    def forward(self, batch):
+        
+        # Get the latent space embedding
+        x = self.get_latent_space_embeddings(batch)
+
+        # Final step to produce logits
+        x = self.relu(x)
+        logits = self.fc_out(x)
+
+        return logits
