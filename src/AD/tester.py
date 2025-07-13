62b0ec7bdd3adfb57a5553d116903b815bbea9d8
ved <vedshah.email@gmail.com>
2025-07-13 01:13:22 -0500

    Added make_AD_UMAP_plots and create_time_evolved_umap_plot in tester.py for plotting latent space representation with anomalies

--- src/AD/tester.py
+++ src/AD/tester.py
@@ -1,9 +1,11 @@
+import umap
 import torch
 import joblib
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
 
 from sklearn.metrics import classification_report
 from tqdm import tqdm
@@ -18,6 +20,7 @@ class Tester:
         self.model_dir = model_dir
         self.one_hot_encoder = joblib.load(f"{self.model_dir}/encoder.pkl")
         self.device = device
+        self.eval()
 
     def create_loss_history_plot(self):
 
@@ -71,17 +74,14 @@ class Tester:
         combined_df = pd.concat(day_wise_metrics, axis=1, join='inner')
         combined_df = combined_df.reindex(sorted(combined_df.columns), axis=1)
         return combined_df
-
-    def run_all_analysis(self, test_loader, d):
-
-        self.eval()
+    
+    def run_test_loop(self, test_loader):
 
         true_classes = []
+        bts_classes = []
         combined_pred_df = []
-        combined_true_df = []
         combined_embeddings = []
-
-        print(f'==========\nStarting Analysis for Trigger + {d} days...')
+        ztf_ids = []
 
         # Run inference on the test set and combine the output dataframes
         for batch in tqdm(test_loader, desc='Testing'):
@@ -93,29 +93,36 @@ class Tester:
             pred_df = self.predict_conditional_probabilities_df(batch)
             embeddings = pd.DataFrame(self.get_latent_space_embeddings(batch).detach())
 
-            # Make dataframe for true labels
-            true_df = self.one_hot_encoder.transform(np.asarray(batch['label']).reshape(-1, 1)).toarray()
-            true_df = pd.DataFrame(true_df, columns=pred_df.columns)
-
             # Maintain list across all batches
             true_classes += batch['label'].tolist()
+            bts_classes += batch['bts_class'].tolist()
+            ztf_ids += batch['ZTFID'].tolist()
             combined_pred_df.append(pred_df)
-            combined_true_df.append(true_df)
             combined_embeddings.append(embeddings)
         
         true_classes = np.array(true_classes)
+        bts_classes = np.array(bts_classes)
         combined_pred_df = pd.concat(combined_pred_df, ignore_index=True)
-        combined_true_df = pd.concat(combined_true_df, ignore_index=True)
         combined_embeddings = pd.concat(combined_embeddings, ignore_index=True)
+
+        return true_classes, bts_classes, combined_pred_df, combined_embeddings
+
+
+    def run_all_analysis(self, test_loader, d):
+
+        print(f'==========\nStarting Analysis for Trigger + {d} days...')
+        true_classes, _, combined_pred_df, combined_embeddings = self.run_test_loop(test_loader)     
+
+        # Make dataframe for true labels encodings
+        combined_true_df = self.one_hot_encoder.transform(true_classes.reshape(-1, 1)).toarray()
+        combined_true_df = pd.DataFrame(combined_true_df, columns=combined_pred_df.columns)
 
         # Make dirs for plots and reports
         Path(f"{self.model_dir}/plots/").mkdir(parents=True, exist_ok=True)
         Path(f"{self.model_dir}/reports/").mkdir(parents=True, exist_ok=True)
 
-        # Get all the nodes at depth 
         classes = np.asarray(self.one_hot_encoder.categories_[0])
         pred_classes = classes[np.argmax(combined_pred_df.to_numpy(), axis=1)]
-        true_classes = classes[np.argmax(combined_true_df.to_numpy(), axis=1)]
         
         title = f"Trigger+{d} days"
 
@@ -149,10 +156,47 @@ class Tester:
         report = self.create_classification_report(np.array(true_classes), np.array(pred_classes), report_file)
         print(report)
 
+    def make_AD_UMAP_plots(self, test_loader, d):
+
+        true_classes, bts_classes, _, combined_embeddings = self.run_test_loop(test_loader)     
+        title = f"Trigger+{d} days"
+
+        # Make the umap plots
+        Path(f"{self.model_dir}/plots/umap_AD").mkdir(parents=True, exist_ok=True)
+        umap_img_file = f"{self.model_dir}/plots/umap_AD/umap_ad_trigger+{d}.pdf"
+        plot_latent_space_umap(combined_embeddings, true_classes, title, umap_img_file)
+
+        # Make the tsne plots
+        Path(f"{self.model_dir}/plots/tsne_AD").mkdir(parents=True, exist_ok=True)
+        tsne_img_file = f"{self.model_dir}/plots/tsne_AD/tsne_ad_trigger+{d}.pdf"
+        plot_latent_space_tsne(combined_embeddings, true_classes, title, tsne_img_file)
+
+        # Write down the embeddings to disk
+        Path(f"{self.model_dir}/embeddings/").mkdir(parents=True, exist_ok=True)
+        combined_embeddings['class'] = true_classes
+        combined_embeddings['days_since_trigger'] = [d]*len(true_classes)
+        combined_embeddings['bts_class'] = bts_classes
+        combined_embeddings.to_csv(f"{self.model_dir}/embeddings/trigger+{d}.csv", index=False)
 
 
+    def create_time_evolved_umap_plot(self, days):
 
+        combined_df = []
+        for d in days:
+            df = pd.read_csv(f"{self.model_dir}/embeddings/trigger+{d}.csv")
+            combined_df.append(df)
 
+        combined_df = pd.concat(combined_df)
 
+        embeddings = combined_df.to_numpy()[:, :-3]
+
+        reducer = umap.UMAP(random_state=42)
+        umap_embedding = reducer.fit_transform(embeddings)
+        combined_df['umap1'] = umap_embedding[:, 0]
+        combined_df['umap2'] = umap_embedding[:, 1]
+        combined_df['log_days'] = np.log2(combined_df['days_since_trigger']) + 1
+
+        fig = px.scatter(combined_df, x='umap1', y='umap2', color=f"class", size='log_days', hover_data=['class','days_since_trigger', 'bts_class'])#, cmap='viridis', marker=markers[i])
+        fig.write_html(f"{self.model_dir}/interactive_umap.html")
 
 
