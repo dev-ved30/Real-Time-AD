Fixed divide by zero error for normalization

src/AD/custom_datasets/BTS.py
@@ -405,8 +405,15 @@ def truncate_BTS_light_curve_fractionally(x_ts, x_static, f=None, normalize_flux
 
     # Normalize the time series
     if normalize_flux:
+
         flux_index = time_dependent_feature_list.index('flux')
-        x_ts[:, flux_index] = (x_ts[:, flux_index] - torch.mean(x_ts[:, flux_index])) / torch.std(x_ts[:, flux_index])
+        mean = torch.mean(x_ts[:, flux_index])
+        std = torch.std(x_ts[:, flux_index])
+        
+        if std > 0: 
+            x_ts[:, flux_index] = (x_ts[:, flux_index] - mean) / std
+        else:
+            x_ts[:, flux_index] = (x_ts[:, flux_index] - mean)
 
     return x_ts, x_static
 
@@ -437,7 +444,14 @@ def truncate_BTS_light_curve_by_days_since_trigger(x_ts, x_static, d=None, add_j
 
     # Normalize the time series
     if normalize_flux:
-        x_ts[:, flux_index] = (x_ts[:, flux_index] - torch.mean(x_ts[:, flux_index])) / torch.std(x_ts[:, flux_index])
+
+        mean = torch.mean(x_ts[:, flux_index])
+        std = torch.std(x_ts[:, flux_index])
+        
+        if std > 0: 
+            x_ts[:, flux_index] = (x_ts[:, flux_index] - mean) / std
+        else:
+            x_ts[:, flux_index] = (x_ts[:, flux_index] - mean)
 
     return x_ts, x_static
 

src/AD/custom_datasets/ZTF_sims.py
@@ -278,9 +278,16 @@ def truncate_ZTF_SIM_light_curve_by_days_since_trigger(x_ts, d, normalize_flux=T
 
     # Normalize the time series
     if normalize_flux:
-        flux_index = time_dependent_feature_list.index('FLUXCAL')
-        x_ts[:, flux_index] = (x_ts[:, flux_index] - torch.mean(x_ts[:, flux_index])) / torch.std(x_ts[:, flux_index])
 
+        flux_index = time_dependent_feature_list.index('FLUXCAL')
+        mean = torch.mean(x_ts[:, flux_index])
+        std = torch.std(x_ts[:, flux_index])
+        
+        if std > 0: 
+            x_ts[:, flux_index] = (x_ts[:, flux_index] - mean) / std
+        else:
+            x_ts[:, flux_index] = (x_ts[:, flux_index] - mean)
+            
     return x_ts
 
 def truncate_ZTF_SIM_light_curve_fractionally(x_ts, f=None, normalize_flux=True):
@@ -301,8 +308,15 @@ def truncate_ZTF_SIM_light_curve_fractionally(x_ts, f=None, normalize_flux=True)
 
     # Normalize the time series
     if normalize_flux:
+
         flux_index = time_dependent_feature_list.index('FLUXCAL')
-        x_ts[:, flux_index] = (x_ts[:, flux_index] - torch.mean(x_ts[:, flux_index])) / torch.std(x_ts[:, flux_index])
+        mean = torch.mean(x_ts[:, flux_index])
+        std = torch.std(x_ts[:, flux_index])
+
+        if std > 0: 
+            x_ts[:, flux_index] = (x_ts[:, flux_index] - mean) / std
+        else:
+            x_ts[:, flux_index] = (x_ts[:, flux_index] - mean)
 
     return x_ts
 